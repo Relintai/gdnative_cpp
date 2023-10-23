e468d94bbedf9275fa22a3a0972ae50a84550ee1
Relintai <relintai@protonmail.com>
2023-10-23 19:30:52 +0200

    Codestyle.

--- core/wrapped.h
+++ core/wrapped.h
@@ -1,3 +1,6 @@
+#ifndef WRAPPED_H
+#define WRAPPED_H
+
 /*************************************************************************/
 /*  wrapped.h                                                          */
 /*************************************************************************/
@@ -27,9 +30,6 @@
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
-
-#ifndef WRAPPED_H
-#define WRAPPED_H
 
 #include <gdn/gdnative.h>
 
